204f7d34da77bbb05ec76b36d234a7d051846328
dafeng.xdf <dafeng.xdf@taobao.com>
2014-08-12 16:52:18 +0800

    First Commit

---
+++ test/generator-demo.test.js
@@ -0,0 +1,19 @@
+/* ================================================================
+ * generator-demo by xdf(xudafeng[at]126.com)
+ *
+ * first created at : Tue Aug 12 2014 09:15:18 GMT+0800 (CST)
+ *
+ * ================================================================
+ * Copyright 2013 xdf
+ *
+ * Licensed under the MIT License
+ * You may not use this file except in compliance with the License.
+ *
+ * ================================================================ */
+
+"use strict";
+
+var generator-demo = require('../');
+
+describe('', function(){
+});
